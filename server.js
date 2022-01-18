0c2425df8b3c09f58e82ec2602cecf3b834b987e
anbav <vibu.anbarasan@gmail.com>
2022-01-18 13:29:16 -0500

    up to date

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 require('dotenv').config()
 
 const Hapi = require('@hapi/hapi')
+const Path = require('path')
+const Inert = require('@hapi/inert')
 
 const init = async () => {
 
   const server = Hapi.server({
     port: process.env.PORT,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'public')
+      }
+    }
   })
+
+  await server.register(Inert)
 
   server.route({
     method: 'GET',
     path: '/',
     handler: (request, h) => {
-
-        return 'Hello World!'
+      return h.file('index.html')
     }
   })
 
