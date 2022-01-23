e8093f5e7f5fbee314435510728f64f95de09117
anbav <vibu.anbarasan@gmail.com>
2022-01-22 22:55:14 -0500

    working button for menu items

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('vibu is logging')
+let dash = 1
+let size = 20
+
+function setup () {
+  pixelDensity(window.devicePixelRatio)
+  createCanvas(50, 50)
+}
+
+function draw () {
+  background('white')
+  drawingContext.setLineDash([dash, dash])
+  ellipse(25, 25, size)
+}
+
+function mousePressed() {
+  if (dist(mouseX, mouseY, 25, 25) <= size) {
+    if (dash === 1) {
+      dash = 3
+      size = 23
+    } else {
+      dash = 1
+      size = 20
+    }
+  }
+}
