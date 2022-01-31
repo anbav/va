c745abc453ac80cfc64ab53bee6fcc78539c6c46
anbav <vibu.anbarasan@gmail.com>
2022-01-31 18:25:52 -0500

    rm'd es lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,3 @@
-// let dash = 1
-// let size = 20
-
-// function setup () {
-//   pixelDensity(window.devicePixelRatio)
-//   createCanvas(50, 50)
-// }
-
-// function draw () {
-//   background('white')
-//   drawingContext.setLineDash([dash, dash])
-//   ellipse(25, 25, size)
-// }
-
-// function mousePressed() {
-//   if (dist(mouseX, mouseY, 25, 25) <= size) {
-//     if (dash === 1) {
-//       dash = 3
-//       size = 23
-//     } else {
-//       dash = 1
-//       size = 20
-//     }
-//   }
-// }
-
 import { Menu } from './menu.js'
 
 const newDiv = document.createElement('div')
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,22 @@
+const MenuItems = () => {
+}
+
 export const Menu = () => {
   const centerdDiv = document.createElement('div')
 
   const newDiv = document.createElement('div')
 
+  const imgEl = document.createElement('img')
+
   newDiv.innerHTML = 'vibu anbarasan is a designer, developer based out of nyc.'
   newDiv.setAttribute('class', 'vibu')
   newDiv.onclick = () => { alert('clicking') }
 
+  imgEl.src = '../../assets/selected.svg'
+
   centerdDiv.setAttribute('class', 'center-align')
   centerdDiv.appendChild(newDiv)
+  centerdDiv.appendChild(imgEl)
 
   return centerdDiv
 }
