05474da4783cc1f78731aaec639ea758d0772a93
anbav <vibu.anbarasan@gmail.com>
2022-01-31 22:36:39 -0500

    working responsive button group

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,22 +1,44 @@
 const MenuItems = () => {
+  const items = ['info', 'tools', 'sup']
+
+  let active = 'info'
+
+  const createButton = (id) => {
+    const button = document.createElement('button')
+    button.setAttribute('id', id)
+    button.setAttribute('class', (active === id) ? 'button-active' : 'button')
+    button.onclick = () => { 
+      items.filter(i => i !== id).forEach(item => {
+        let el = document.querySelector(`#${item}`)
+        if (el.classList.contains('button-active')) {
+          el.classList.remove('button-active')
+          el.classList.add('button')
+        } else {
+          let el = document.querySelector(`#${id}`)
+          el.classList.remove('button')
+          el.classList.add('button-active')
+        }
+      })
+    }
+
+    return button
+  }
+
+  return items.map(items => createButton(items))
 }
 
 export const Menu = () => {
   const centerdDiv = document.createElement('div')
-
   const newDiv = document.createElement('div')
-
-  const imgEl = document.createElement('img')
+  const menuBtns = MenuItems()
 
   newDiv.innerHTML = 'vibu anbarasan is a designer, developer based out of nyc.'
   newDiv.setAttribute('class', 'vibu')
   newDiv.onclick = () => { alert('clicking') }
 
-  imgEl.src = '../../assets/selected.svg'
-
   centerdDiv.setAttribute('class', 'center-align')
   centerdDiv.appendChild(newDiv)
-  centerdDiv.appendChild(imgEl)
+  menuBtns.forEach(item => centerdDiv.appendChild(item))
 
   return centerdDiv
 }
