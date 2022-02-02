af81ec1a4a7598d6495438d3361d62fce0cc7150
anbav <vibu.anbarasan@gmail.com>
2022-02-02 00:24:58 -0500

    working menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,44 +1,62 @@
 const MenuItems = () => {
+  const wrapper = document.createElement('div')
+
   const items = ['info', 'tools', 'sup']
 
   let active = 'info'
 
   const createButton = (id) => {
-    const button = document.createElement('button')
-    button.setAttribute('id', id)
-    button.setAttribute('class', (active === id) ? 'button-active' : 'button')
+    const button = document.createElement('div')
+    const dial = document.createElement('button')
+    const text = document.createElement('p')
+
+    button.setAttribute('class', 'menu-item')
     button.onclick = () => { 
       items.filter(i => i !== id).forEach(item => {
         let el = document.querySelector(`#${item}`)
-        if (el.classList.contains('button-active')) {
-          el.classList.remove('button-active')
-          el.classList.add('button')
+        if (el.classList.contains('dial-active')) {
+          el.classList.remove('dial-active')
+          el.classList.add('dial')
         } else {
           let el = document.querySelector(`#${id}`)
-          el.classList.remove('button')
-          el.classList.add('button-active')
+          el.classList.remove('dial')
+          el.classList.add('dial-active')
         }
       })
     }
 
+    dial.setAttribute('id', id)
+    dial.setAttribute('class', (active === id) ? 'dial-active' : 'dial')
+
+    text.setAttribute('class', 'button-text')
+    text.innerHTML = `${id === 'sup' ? 'sup?' : id}`
+
+    button.appendChild(dial)
+    button.appendChild(text)
+
     return button
   }
 
-  return items.map(items => createButton(items))
+  wrapper.setAttribute('class', 'center-align')
+
+  items.map(item => createButton(item)).forEach(i => {
+    wrapper.appendChild(i)
+  })
+
+  return wrapper
 }
 
 export const Menu = () => {
   const centerdDiv = document.createElement('div')
   const newDiv = document.createElement('div')
-  const menuBtns = MenuItems()
+  const menu = MenuItems()
 
-  newDiv.innerHTML = 'vibu anbarasan is a designer, developer based out of nyc.'
+  newDiv.innerHTML = `<p class='vibu-text'>vibu anbarasan is a designer, developer based out of nyc.</p>`
   newDiv.setAttribute('class', 'vibu')
-  newDiv.onclick = () => { alert('clicking') }
 
   centerdDiv.setAttribute('class', 'center-align')
   centerdDiv.appendChild(newDiv)
-  menuBtns.forEach(item => centerdDiv.appendChild(item))
+  centerdDiv.appendChild(menu)
 
   return centerdDiv
 }
